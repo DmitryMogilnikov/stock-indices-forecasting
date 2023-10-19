0651387896dc94decaa8d4027f474b1e2b32117d
almostinf <almostinf@mail.ru>
2023-10-19 23:49:29 +0300

    add function for calculating days to target reduction and tests for it

---
+++ backend/src/service/ts.py
@@ -0,0 +1,29 @@
+from typing import List
+
+def is_reduction_sufficient(initial_percent: float, percent_value: float, target_reduction: float, tolerance: float = 1e-6) -> bool:
+    reduction_difference = initial_percent - percent_value
+    return initial_percent > percent_value and reduction_difference - target_reduction > tolerance
+
+def get_days_to_reduce_procent(percentage_changes: List[float], initial_percent: float, target_reduction: float, tolerance: float = 1e-6) -> int:
+    if len(percentage_changes) == 0:
+        return 0
+
+    days: int = 0
+
+    for percent_change in percentage_changes:
+        days += 1
+        if is_reduction_sufficient(initial_percent, percent_change, target_reduction, tolerance):
+            break
+
+    if days == len(percentage_changes) and not is_reduction_sufficient(initial_percent, percentage_changes[-1], target_reduction, tolerance):
+        days = 0
+
+    return days
+
+def calculate_days_to_target_reductions(percentage_changes: List[float], target_reduction: float, tolerance: float = 1e-6) -> List[int]:
+    days: List[int] = []
+
+    for i, percentage_change in enumerate(percentage_changes):
+        days.append(get_days_to_reduce_procent(percentage_changes[i+1:], percentage_change, target_reduction, tolerance))
+
+    return days
