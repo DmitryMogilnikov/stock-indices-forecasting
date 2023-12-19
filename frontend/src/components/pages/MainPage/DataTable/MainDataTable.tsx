976254cab63cfb1a77912d3a2f9ac3cae100c2cb
Kordebalet232 <yurypetrovich2013@mail.ru>
2023-12-19 16:07:32 +0300

    Добавлены прогнозы и графики

--- frontend/src/components/pages/MainPage/DataTable/MainDataTable.tsx
+++ frontend/src/components/pages/MainPage/DataTable/MainDataTable.tsx
@@ -12,28 +12,40 @@ const MainDataTable = (props: Props) => {
         <div className="Main_DataTable">
             <table id="dataTable"  className="dataTable">
                 <caption>Сформированные данные</caption>
-                <tr>
-                    <th>Дата</th>
-                    <th>Цена</th>
-                    <th>Откр.</th>
-                    <th>Макс.</th>
-                    <th>Мин.</th>
-                    <th>Интегральная<br></br>сумма</th>
-                    <th>Процентный<br></br>прирост</th>
-                    <th>Продолжительность<br></br>снижения</th>
-                </tr>
-                {props.data ? props.data.map((row) => (
+                <thead>
                     <tr>
-                        <th>{row[0]}</th>
-                        <th>{row[2]}</th>
-                        <th>{row[1]}</th>
-                        <th>{row[4]}</th>
-                        <th>{row[3]}</th>
-                        <th>{row[5].toFixed(2)}</th>
-                        <th>{row[6].toFixed(2)}</th>
-                        <th>{row[7]}</th>
+                        <th>Дата</th>
+                        <th>Цена</th>
+                        <th>Откр.</th>
+                        <th>Макс.</th>
+                        <th>Мин.</th>
+                        <th>Интегральная<br></br>сумма</th>
+                        <th>Процентный<br></br>прирост</th>
+                        <th>Продолжительность<br></br>снижения</th>
+                        <th>Прогнозируемый<br></br>процент</th>
+                        <th>Прогнозируемая<br></br>интегральная<br></br>сумма</th>
+                        <th>Прогнозируемая<br></br>цена</th>
+                        <th>ошибка</th>
                     </tr>
-                )) : <></>}
+                </thead>
+                <tbody>
+                    {props.data ? props.data.map((row) => (
+                        <tr>
+                            <th>{row[0]}</th> {/* Дата */}
+                            <th>{row[2]}</th> {/* Цена */}
+                            <th>{row[1]}</th> {/* Откр */}
+                            <th>{row[4]}</th> {/* Макс */}
+                            <th>{row[3]}</th> {/* Мин */}
+                            <th>{row[5].toFixed(2)}</th> {/* Интегральная сумма */}
+                            <th>{row[6].toFixed(2)}</th> {/* Процентный прирост */}
+                            <th>{row[7]}</th> {/* Продолжительность снижения */}
+                            <th>{row[8].toFixed(2)}</th> {/* Прогнозируемый процент */}
+                            <th>{row[9].toFixed(2)}</th> {/* Прогнозируемая сумма */}
+                            <th>{row[10].toFixed(2)}</th> {/* Прогнозируемая цена */}
+                            <th>{row[11].toFixed(2)}</th> {/* Ошибка */}
+                        </tr>
+                    )) : <></>}
+                </tbody>
             </table>
         </div>
     )
